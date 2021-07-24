e2c75c793613a37b384ea8e467029b15bda59bdf
Henrique Prado <henrique.mxnero@gmail.com>
2021-07-24 20:21:58 -0300

    Desenvolvimento da tela do resultado do quiz

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function showQuestion() {
         });
 
     } else {
-        // acabaram as questões
+        finishQuiz();
     }
 }
 
@@ -42,4 +42,29 @@ function optionClickEvent(e) {
 
     currentQuestion++;
     showQuestion();
+}
+
+
+function finishQuiz() {
+
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if (points < 30) {
+        document.querySelector('.scoreText1').innerHTML = 'Tá ruim em?!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if (points >= 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!';
+        document.querySelector('.scorePct').style.color = '#0D630D';
+    }
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`;
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+
 }
